894e0afb1bda9f239e386c5a7bb18f480b77adbe
VIKRAM009KILLER <81997240+VIKRAM009KILLER@users.noreply.github.com>
2021-07-21 08:43:39 +0530

    Add files via upload

---
+++ getinrange.py
@@ -0,0 +1,26 @@
+# Write the function getInRange(x, bound1, bound2) which takes 3 int values
+# x, bound1, and bound2, where bound1 is not necessarily less than bound2. 
+# If x is between the two bounds, just return it unmodified. Otherwise, 
+# if x is less than the lower bound, return the lower bound, 
+# or if x is greater than the upper bound, return the upper bound.
+
+def fun_getinrange(x, bound1, bound2):
+	# your code goes here
+	if bound1 < bound2:
+		if x > bound1:
+			if x > bound2:
+				return bound2
+			else:
+				return x
+		else:
+			return bound1
+
+	else:
+		if x > bound2:
+			if x > bound1:
+				return bound1
+			else:
+				return x
+		else:
+			return bound2
+	#return -1
