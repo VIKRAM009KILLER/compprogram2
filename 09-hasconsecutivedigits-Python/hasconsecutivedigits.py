a970457107cb3cb449a3525023578cfec257bd34
HARSHCHANDRA009 <harshchandra008@msitprogram.net>
2021-07-26 23:20:07 +0530

    conec digits

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -4,4 +4,18 @@
 
 def hasconsecutivedigits(n):
 	# your code goes here
+	n = abs(n)
+	if n < 10:
+		return False
+	ref = n % 10
+	n = n // 10
+	while(n != 0):
+		m = n % 10
+		if ref == m:
+			return True
+		else:
+			ref = m
+		n = n // 10
+	return False
+
 	pass
