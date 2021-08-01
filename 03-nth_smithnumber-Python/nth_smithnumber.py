c466bbcfe9762f65f9d668be001f0eb8bfa46321
HARSHCHANDRA009 <harshchandra008@msitprogram.net>
2021-08-01 08:42:32 +0530

    smithno.

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,6 +7,57 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
+def isPrime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def digisum(n):
+    if n < 10:
+        return n
+    sum = 0
+    while(n >0):
+        d = n % 10
+        n = n // 10
+
+        sum += d
+
+    return sum
+
+def isSmith(n):
+    sum = digisum(n)
+    sum1 = 0
+    for i in range(2, n):
+        if n % i == 0:
+            if(isPrime(i)):
+                sd = digisum(i)
+                factor = 0
+                num = n
+                while(num%i==0):
+                    factor += 1
+                    num = num // i
+                
+
+                sum1 += factor * sd
+            
+    if sum == sum1:
+        return True
+    else:
+        return False
 
 def fun_nth_smithnumber(n):
+    if n == 0:
+        return 4
+    m = 10 ** 10
+    count = 0
+    for i in range(5, m):
+        if(isPrime(i) == False):
+            if(isSmith(i)):
+                count += 1
+
+        if count == n:
+            return i
     return 1
