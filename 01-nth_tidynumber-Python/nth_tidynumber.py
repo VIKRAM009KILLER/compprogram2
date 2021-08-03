3e940f3327ade5f202b740dcabba25f1739ea360
HARSHCHANDRA009 <harshchandra008@msitprogram.net>
2021-08-03 15:50:44 +0530

    tidynumber

--- 01-nth_tidynumber-Python/nth_tidynumber.py
+++ 01-nth_tidynumber-Python/nth_tidynumber.py
@@ -5,6 +5,31 @@
 # fun_nth_tidynumber(5) = 6
 # fun_nth_tidynumber(15) = 17
 # fun_nth_tidynumber(35) = 46
+def isTidy(n):
+    num = str(n)
+    oldlist = []
+    for i in num:
+        oldlist.append(i)
+    
+    oldlist.sort()
+    res = ''
+    finnum = res.join(oldlist)
+    if finnum == num:
+        return True
+    else:
+        return False
+    
+
 
 def fun_nth_tidynumber(n):
+    if n <= 8:
+        return n+1
+    m = 10**50
+    count = 8
+    for i in range(11, m):
+        if(isTidy(i)):
+            count += 1
+        if count == n:
+            return i
+
     return 0
