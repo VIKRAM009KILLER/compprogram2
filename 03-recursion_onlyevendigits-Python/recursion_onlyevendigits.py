de173ab661f391ad6c41975c14eae960bbf1de79
HARSHCHANDRA009 <harshchandra008@msitprogram.net>
2021-08-03 08:28:24 +0530

    evendigitsonly

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -7,6 +7,40 @@
 # So: onlyEvenDigits([43, 23265, 17, 58344]) returns [4, 226, 0, 844]. 
 # Also the function returns the empty list if the original list is empty. 
 # Remember to not use strings. You may not use loops/iteration in this problem.
+def removeodd(n, s):
+    if n == 0:
+        return 0
+    num = n % 10
 
-def fun_recursion_onlyevendigits(l): 
+    # print(n)
+    
+        
+    n = n // 10
+    if num % 2 == 0:
+            
+        c = s - len(str(n))
+        if n == 0:
+            c = s
+        
+        # print(len(str(n)))
+        # print(s)
+        # print(c-1)
+        return num * 10**(c-1) + removeodd(n, s)
+    else:
+        
+        
+        s = s - 1
+        # print(s)
+        return 0 + removeodd(n, s)
+
+def fun_recursion_onlyevendigits(l):
+	if len(l) == 0:
 		return []
+	num = l.pop(0)
+	newnum = removeodd(num, len(str(num)))
+		
+
+	
+
+	return [newnum] + fun_recursion_onlyevendigits(l)
+		
