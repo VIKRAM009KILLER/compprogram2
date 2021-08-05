65f388f0b5b5e9f3396da8fcd3e1d053458fbf34
HARSHCHANDRA009 <harshchandra008@msitprogram.net>
2021-08-05 21:27:46 +0530

    04-longestdigitrun-Python

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -6,4 +6,53 @@
 # as does longestDigitRun(-677886).
 def longestdigitrun(n):
 	# Your code goes here
+	n = abs(n)
+	x = str(n)
+	maxcount = 0
+	count = 0
+	maxnum = '0'
+	new = []
+	for i in x:
+		new.append(i)
+	dnew = list(set(new))
+	new.append("test")
+# 	print(dnew)
+# 	print(new)
+# 	print('Start')
+
+
+	for i in dnew:
+# 		print(i)
+# 		print('iteration')
+		count = 0
+		for j in new:
+			if i == j:
+				count += 1
+			else:
+			 #   print(count)
+			    if count > maxcount:
+			        maxcount = count
+			        maxnum = i
+			       
+			    elif count == maxcount and maxcount != 0:
+			        if maxnum > i:
+			            maxnum = i
+			        else:
+			            maxnum = maxnum
+			        
+			 #   print(j)
+			 #   print(maxcount)
+			 #   print(maxnum)
+			 #   print(i)
+			 #   print("End")
+			    count = 0
+			    
+				
+				
+		continue
+
+	return int(maxnum)
+	
+# print(longestdigitrun(117773732))
+
 	pass
