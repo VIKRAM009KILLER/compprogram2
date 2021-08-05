66ff77d7489bb00fde8cc9bcfbd01e7139b9f27e
HARSHCHANDRA009 <harshchandra008@msitprogram.net>
2021-08-05 12:25:26 +0530

    median

--- 09-median-Python/median.py
+++ 09-median-Python/median.py
@@ -5,4 +5,19 @@
 
 def median(a):
 	# your code goes here
+	if a == []:
+		return None
+	if len(a) == 1:
+		return a[0]
+	if len(a) % 2 == 0:
+		n = len(a) // 2
+		i = n-1
+		j = n
+		m = (a[i]+a[j]) / 2
+
+	else:
+		n = len(a) // 2
+		m = a[n]
+
+	return m
 	pass
