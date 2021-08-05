4213f20b82fed353f9a79d7e56f6c65eaa501b5c
HARSHCHANDRA009 <harshchandra008@msitprogram.net>
2021-08-05 13:38:33 +0530

    addyprime

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -4,7 +4,45 @@
 # is also prime, so 113 is an Additive Prime. fun_nth_additive_prime(0) returns 2
 
 
+def digisum(n):
+	x = str(n)
+	num = 0
+	for i in x:
+		num += int(i)
 
+	return num
+
+def isPrime(n):
+	if n <= 1:
+		return False
+	for i in range(2, n):
+		if n % i == 0:
+			return False
+	return True 
 
 def fun_nth_additive_prime(n):
+	if n == 0:
+		return 2
+	if n == 1:
+		return 3
+	if n == 2:
+		return 5
+	if n == 3:
+		return 7
+	count = 3
+	
+	m = 10 ** 50
+
+	for i in range(11, m):
+		num = digisum(i)
+		if(isPrime(num)):
+			if(isPrime(i)):
+				count += 1
+		if count == n:
+			return i
+
+
+
+
+
 	return 1
