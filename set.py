42a1929a1804fa7060104c0952f432e60841629e
VIKRAM009KILLER <81997240+VIKRAM009KILLER@users.noreply.github.com>
2021-08-06 16:26:14 +0530

    limitedpowerset

--- set.py
+++ set.py
@@ -9,4 +9,21 @@
 
 def limitedPowerSet(n, k):
     # Your code goes here...
+    newset = [{}]
+    for i in range(1, n+1):
+        a = {i}
+        newset.append(a)
+        
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if(len(newset)) == k:
+                return newset
+            else:
+                if j != 1:
+                    y = (i, j)
+                    newset.append(y)
+                    
+    assert(limitedPowerSet(6, 10) == [{}, {1}, {2}, {3}, {4}, {5}, {6}, {1, 2}, {1, 3}, {1, 4}]
+           
+        
     pass
