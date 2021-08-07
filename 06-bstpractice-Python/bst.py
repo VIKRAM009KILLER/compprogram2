4411312fb3aa900b0c2897402dcc56cff6f03f3a
VIKRAM009KILLER <81997240+VIKRAM009KILLER@users.noreply.github.com>
2021-08-07 10:06:21 +0530

    Binarytreesearch

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -10,13 +10,44 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
+        num = new_val
+        newnode = Node(num)
+        if self.root == None:
+            self.root = newnode
+        else:
+            current = self.root
+            parent = self.root
+            while(current != None):
+                parent = current
+                if num < current.value:
+                    current = current.left
+                else:
+                    current = current.right
+                    
+            if num < parent.value:
+                parent.left = newnode
+            else:
+                parent.right = newnode
         pass
 
     def printSelf(self):
         # Your code goes here
+        print(self.root)
         pass
         
     def search(self, find_val):
         # Your code goes here
+        num = find_val
+        while(self.root != None):
+            
+            if self.root.value == num:
+                return True
+            if self.root.value < num:
+                self.root = self.root.right
+                
+            elif self.root.value > num:
+                self.root = self.root.right
+                
+            return False
         pass
 
